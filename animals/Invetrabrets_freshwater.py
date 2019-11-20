ea21fb6e224a4b267f32a23d6fd193cfc8fbc14b
rodneysgraphics <2831078+rodneysgraphics@users.noreply.github.com>
2019-11-19 20:04:16 -0500

    Adding in Files for Animal Randomizer
    Adding in files for Animal Randomizer

---
+++ animals/Invetrabrets_freshwater.py
@@ -0,0 +1,3 @@
+invert_fresh = ['Acorne Weevil',' Actinella',' Actinella',' Actinella',' Acuminate Snake tail',' Adonis Blue',' African Babul blue',' African Giant Swllow tail',' Agrias',' Alabama Cave Crayfish',' Alabama cave Shrimp',' Alabama Lampmussel',' Alcorn Large Blue',' Aldabra Bandad Snail',' Alderfly',' Algerian Clubtail',' Ailocharopa',' Alpine Emerals',' Alphine Zypher Blue']
+
+
